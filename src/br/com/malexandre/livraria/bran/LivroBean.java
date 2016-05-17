08c07d16a00f2099e80dcd1fa137ab679a7ecfd9
Marcos Barbosa <malexandrebarbo@gmail.com>
2016-05-17 17:46:31 -0300

    Criando a ação de gravar livro e associando à página. Capturando dados do formulário.

---
+++ src/br/com/malexandre/livraria/bran/LivroBean.java
@@ -0,0 +1,21 @@
+package br.com.malexandre.livraria.bran;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+	
+	private Livro livro = new Livro();
+
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+	}
+
+	
+	
+}
