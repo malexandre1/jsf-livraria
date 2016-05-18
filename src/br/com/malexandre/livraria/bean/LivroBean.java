be85c39e659dbe0c9adb0189b7ed4a9eab267805
Marcos Barbosa <malexandrebarbo@gmail.com>
2016-05-18 17:36:19 -0300

    Autores no combobox, Lista Autores, Lista Livros, Escopo do ManagedBean.

--- src/br/com/malexandre/livraria/bean/LivroBean.java
+++ src/br/com/malexandre/livraria/bean/LivroBean.java
@@ -1,23 +1,53 @@
 package br.com.malexandre.livraria.bean;
 
-import javax.faces.bean.ManagedBean;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.malexandre.livraria.dao.DAO;
+import br.com.malexandre.livraria.modelo.Autor;
 import br.com.malexandre.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 	
 	private Livro livro = new Livro();
+	private Integer autorId;
 
 
 	public Livro getLivro() {
 		return livro;
+	}
+	
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
 	}
 
 	public void gravar() {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 	}
 
+	
 	
 	
 }
